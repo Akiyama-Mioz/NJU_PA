cd51cc42a701a1a5a31abe646a5b0c816d1584f0
tracer-ics2023 <tracer@njuics.org>
2024-07-20 17:14:58 +0800

    >  compile NEMU 233520 Akiyama_Mio Linux DESKTOP-9GSKR19 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:14:58 up  5:30,  1 user,  load average: 0.10, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -101,6 +101,7 @@ static bool make_token(char *e) {
          */
         
         printf("%d",rules[i].token_type);
+        printf("%s",rules[i].regex);
         switch (rules[i].token_type) {
           case TK_NOTYPE : tokens[nr_token].type=TK_NOTYPE;
           printf("%d",rules[i].token_type);break;
