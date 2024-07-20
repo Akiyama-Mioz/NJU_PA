0552eb36f5eac32015bd0497143174c3e32446b9
tracer-ics2023 <tracer@njuics.org>
2024-07-20 17:26:34 +0800

    >  compile NEMU 233520 Akiyama_Mio Linux DESKTOP-9GSKR19 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:26:34 up  5:41,  1 user,  load average: 0.33, 0.14, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -90,8 +90,8 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s %d",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start,e[position]);
 
         position += substr_len;
 
