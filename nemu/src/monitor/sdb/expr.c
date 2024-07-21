167acbda4fa35c95b7187eb55df12d8953a28aab
tracer-ics2023 <tracer@njuics.org>
2024-07-21 16:25:26 +0800

    >  compile NEMU 233520 Akiyama_Mio Linux DESKTOP-9GSKR19 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:25:26 up  5:10,  1 user,  load average: 0.06, 0.06, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -184,6 +184,8 @@ uint32_t eval(int p,int q){
   else{
     int op = main_operator();
     printf("op = %d\n",op);
+    printf("p = %d\n",p);
+    printf("q = %d\n",q);
     assert(0);
     int val1 = eval(p,op-1);
     printf("val1 = %d\n",val1);
