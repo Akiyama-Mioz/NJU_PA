be32b7bce333b5dda65d63223f448bbea20b451d
tracer-ics2023 <tracer@njuics.org>
2024-07-21 12:01:02 +0800

    >  compile NEMU 233520 Akiyama_Mio Linux DESKTOP-9GSKR19 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:01:02 up 45 min,  1 user,  load average: 0.14, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -198,7 +198,7 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   } 
-  eval(0,nr_token);
+  eval(0,nr_token-1);
   /* TODO: Insert codes to evaluate the expression. */
   TODO();
 
